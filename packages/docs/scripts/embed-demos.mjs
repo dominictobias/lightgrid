53bab879cae5d414d32f43de497bdf302978d087
Dominic Tobias <760314+dominictobias@users.noreply.github.com>
2026-06-17 00:51:49 +0100

    Revert "Maybe fix demo asset access issue"
    This reverts commit 3d6b54a590c798b89dd62593ee56a1b467c5736f.

--- packages/docs/scripts/embed-demos.mjs
+++ packages/docs/scripts/embed-demos.mjs
@@ -1,4 +1,4 @@
-import { cp, mkdir, rm } from 'node:fs/promises'
+import { cp } from 'node:fs/promises'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
@@ -6,11 +6,5 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const docsRoot = resolve(__dirname, '..')
 const demoDist = resolve(docsRoot, '../react/dist')
 const demosOutput = resolve(docsRoot, 'build/demos')
-const demoAssetsOutput = resolve(docsRoot, 'build/demo-assets')
 
-await rm(demosOutput, { recursive: true, force: true })
-await rm(demoAssetsOutput, { recursive: true, force: true })
-
-await mkdir(demosOutput, { recursive: true })
-await cp(resolve(demoDist, 'index.html'), resolve(demosOutput, 'index.html'))
-await cp(demoDist, demoAssetsOutput, { recursive: true })
+await cp(demoDist, demosOutput, { recursive: true })
